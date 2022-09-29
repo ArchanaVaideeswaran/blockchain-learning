95f215cd0306e88d8987356b960d568961aaebd2
Archana Vaideeswaran <archanav1929@gmail.com>
2022-09-29 18:33:42 +0530

    script store deployments

--- scripts/PieToke.deploy.ts
+++ scripts/PieToke.deploy.ts
@@ -1,13 +1,43 @@
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import * as fs from "fs";
 
 async function main() {
     const [ owner ] = await ethers.getSigners();
     console.log(owner.address);
 
-    const token = await ethers.getContractFactory("PieToken");
-    const Token = await token.deploy();
+    const Token = await ethers.getContractFactory("PieToken");
+    const token = await Token.deploy();
+    await token.deployed();
+    console.log(token.address);
 
-    console.log(Token.address);
+    storeContract(token);
+}
+
+function storeContract(contract: Contract) {
+    // ----------------- MODIFIED FOR SAVING DEPLOYMENT DATA ----------------- //
+
+    /**
+     * @summary A build folder will be created in the root directory of the project
+     * where the ABI, bytecode and the deployed address will be saved inside a JSON file.
+     */
+
+    const address = contract.address;
+    const contractAbi = contract.interface.format('json').toString();
+    const abi = JSON.parse(contractAbi);
+
+    const output = {
+        address,
+        abi,
+    };
+
+    fs.mkdir('./build', { recursive: true }, (err) => {
+        if (err) console.error(err);
+    });
+
+    fs.writeFileSync('./build/PieToken.json', JSON.stringify(output));
+
+    // ----------------------------------------------------------------------- //
 }
 
 main().catch((error) => {
